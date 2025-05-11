42fdfe37f9c595e27c988d3e3d86138eceb4ae85
emilkadec <s31431@pjwstk.edu.pl>
2025-05-12 00:00:06 +0200

    final

--- Tutorial9/obj/Debug/net9.0/Tutorial9.AssemblyInfo.cs
+++ Tutorial9/obj/Debug/net9.0/Tutorial9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2faedfd3e334a67228c0b824fefbe3c50efb6419")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
